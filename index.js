cb5e6cc023baa686b8ca53ddedfc0577cea87bc2
tmai468 <tmai468@aucklanduni.ac.nz>
2021-11-25 12:20:19 +1300

    update build to latest

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express')
 const path = require('path');
 
 const app = express()
-// app.get('/', (request, response) => {
-//     response.send('<h1>Currency Converter</h1>')
-// })
 const port = process.env.PORT || 3001
-// app.use(express.static(path.join(__dirname, "build")));
 app.use(express.static('build'))
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`))
